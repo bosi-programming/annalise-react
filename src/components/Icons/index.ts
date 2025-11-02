d71bdf84101e169cf2ca0b1555c15a3ea5b1b2a2
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-11-02 01:48:15 -0300

    Add Icons

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export * from './InstagramIcon';
+export * from './BlueskyIcon';
+export * from './TikTokIcon';
+export * from './AmazonIcon';
+export * from './Logo';
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Button';
 export * from './Text';
 export * from './Input';
 export * from './Email';
+export * from './Icons';
