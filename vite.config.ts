ed206d7376655c17656c0bff5266afa1af21b6b0
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-11-02 02:00:08 -0300

    Add @ alias to vite and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
