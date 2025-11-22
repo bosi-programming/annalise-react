41f371ab8b1d9bf0b5f7ad0d39b065f72b00c8c9
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-11-21 21:46:56 -0300

    Set favicon based on user prefered theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import { Footer, Header } from './components';
 import './index.css';
 
 function App() {
+  const prefersLightTheme = window.matchMedia('(prefers-color-scheme: light)');
+
+  if (prefersLightTheme.matches) {
+    const favicon = document.querySelector("link[rel~='icon']");
+    favicon?.setAttribute('href', '/favicon-light.png');
+  }
   return (
     <div className="bg-purple">
       <BrowserRouter>
