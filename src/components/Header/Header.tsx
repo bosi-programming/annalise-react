5e0a179322cebdf9c2c003c2ac07620b8aa3840b
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-11-22 13:33:27 -0300

    New Header version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,22 @@
+import { useWindowSize } from '@/hooks/useWindowSize';
 import { Logo, TikTokIcon } from '../Icons';
 import { Text } from '../Text';
 
 export function Header() {
+  const { windowWidth } = useWindowSize();
+  const isDesktop = windowWidth >= 1024;
   return (
-    <div className="lg:px-13 bg-bg-header flex items-center justify-between px-7 py-4 lg:py-6">
+    <div className="lg:px-13 bg-bg-header flex items-center justify-between px-7 py-6 lg:py-10">
       <div className="flex items-center">
         <Logo />
-        <Text weight="bold" className="text-white">
-          Annalise Cerqueira-Maia
-        </Text>
+        {isDesktop ? (
+          <Text size="h2" weight="bold" className="text-white">
+            Annalise Cerqueira-Maia
+          </Text>
+        ) : null}
       </div>
       <a href="https://www.tiktok.com/@annalisecmaia?is_from_webapp=1&sender_device=pc" target="_blank" rel="external">
-        <TikTokIcon />
+        <TikTokIcon {...(isDesktop ? { size: 40 } : {})} />
       </a>
     </div>
   );
