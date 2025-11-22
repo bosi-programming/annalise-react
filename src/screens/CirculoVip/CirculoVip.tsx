b03cf00ebd48b573dc514523bc6dfe12eff7dd09
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-11-22 14:16:08 -0300

    Change Circulo vip copy to new design

--- src/screens/CirculoVip/CirculoVip.tsx
+++ src/screens/CirculoVip/CirculoVip.tsx
@@ -3,14 +3,14 @@ import { Form, Heading } from './components';
 
 export function CirculoVip() {
   return (
-    <div className="m-auto max-w-7xl">
+    <div className="m-auto max-w-[1440px]">
       <title>Círculo VIP - #ADE</title>
-      <main className="lg:py-45 lg:mb-13 lg:gap-30 lg:px-30 h-full px-8 pt-10 lg:grid lg:grid-cols-2">
+      <main className="lg:py-45 lg:mb-13 lg:gap-30 lg:px-25 h-full max-w-[1440px] px-8 pt-10 lg:grid lg:grid-cols-2">
         <div>
-          <Heading className="leading-none text-white" />
-          <Text className="mr-16 mt-10 text-white">
-            Entre para o Círculo VIP e seja a primeira a receber novidades sobre o universo de #ADE. Prometo pela Lua
-            que brilha no céu: nada de spam.
+          <Heading className="leading-16 text-white" />
+          <Text size="h2" className="mr-16 mt-9 leading-7 text-white">
+            Entre para o Círculo VIP e seja a primeira a receber novidades sobre o universo de A dança dos Encantos.
+            Prometo pela Lua que brilha no céu: nada de spam.
           </Text>
         </div>
         <Form />
