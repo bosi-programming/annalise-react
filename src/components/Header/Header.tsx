88796adc0162e8b5bb3c8bf21a29b0df5db8c7a5
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-11-22 13:41:40 -0300

    Fix Header size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,24 @@ export function Header() {
   const { windowWidth } = useWindowSize();
   const isDesktop = windowWidth >= 1024;
   return (
-    <div className="lg:px-13 bg-bg-header flex items-center justify-between px-7 py-6 lg:py-10">
-      <div className="flex items-center">
-        <Logo />
-        {isDesktop ? (
-          <Text size="h2" weight="bold" className="text-white">
-            Annalise Cerqueira-Maia
-          </Text>
-        ) : null}
+    <div className="bg-bg-header mx-auto flex justify-center">
+      <div className="lg:px-25 grow-1 flex max-w-[1440px] items-center justify-between px-7 py-6 lg:py-10">
+        <div className="flex items-center">
+          <Logo />
+          {isDesktop ? (
+            <Text size="h2" weight="bold" className="text-white">
+              Annalise Cerqueira-Maia
+            </Text>
+          ) : null}
+        </div>
+        <a
+          href="https://www.tiktok.com/@annalisecmaia?is_from_webapp=1&sender_device=pc"
+          target="_blank"
+          rel="external"
+        >
+          <TikTokIcon {...(isDesktop ? { size: 40 } : {})} />
+        </a>
       </div>
-      <a href="https://www.tiktok.com/@annalisecmaia?is_from_webapp=1&sender_device=pc" target="_blank" rel="external">
-        <TikTokIcon {...(isDesktop ? { size: 40 } : {})} />
-      </a>
     </div>
   );
 }
